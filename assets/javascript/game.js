5f1ee2c9dd13491d125c8ed97cd9033214397459
Orvin Welchez <orvinwelchez@Orvins-MacBook-Pro.local>
2016-05-24 11:05:30 -0400

    Anything

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
-<script type="text/javascript">
-document.onkeyup = function(event) {
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+//document.onkeyup = function(event) {
+//	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
 
 
 var gamesList = ["punchout", "mariobros", "legendofzelda", "contra", "donkeykong", 
 				"balloonfight", "bomberman", "excitebike", "twinbee", "xevious"];
 
-var randomGame = gamesList[Math.floor(Math.random() * gamesList.lenght)];
+var gameNameSize = 
+
+var activeGame = "";
+
+
+//var randomGame = gamesList[Math.floor(Math.random() * gamesList.lenght)];
 
 
 
 
-</script>
